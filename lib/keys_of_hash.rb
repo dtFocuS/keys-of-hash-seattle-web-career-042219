290d986c4d8b34558fe2341509ea8bb2da68160b
Danny Tseng <dtfocus1025@gmail.com>
2019-04-11 05:58:21 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,7 +5,7 @@ class Hash
     arguments.each do |target_value|
       keys.each do |target_key|
         values_at(target_key).each do |value|
-          array << target_value == value
+          array << target_value == value[0]
         end
       end
     end
