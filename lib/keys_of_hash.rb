61d518ea60b720af89e84df4ac3c680026619bf3
Danny Tseng <dtfocus1025@gmail.com>
2019-04-11 05:15:22 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,9 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     array = []
-    arguments.each {|value| array << key(value) }
+    arguments.each do |value| 
+      array << key(value)
+    end
     array
   end
 end
